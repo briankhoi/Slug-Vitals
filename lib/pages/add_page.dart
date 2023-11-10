935fc2489d65f537a6d60f579b99387edf5f8fc8
Adam Owyang <>
2023-11-10 15:28:26 -0800

    font config

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -2,7 +2,29 @@ import 'package:flutter/material.dart';
 
 class AddPage extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
-    return Center(child: Text('add Page'));
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Nexa'),
+      title: 'Cruz Nibbles',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Cruz Nibbles'),
+        ),
+        body: GridView.count(
+          // create two columns
+          crossAxisCount: 2,
+          // Generate 5 widgets, one for each dining hall
+          children: List.generate(5, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+            );
+          }),
+        ),
+      ),
+    );
   }
 }
