2c6dec56fa6ab08d6462a19aaf2e95c9227e0ea2
Brian <86499417+briankhoi@users.noreply.github.com>
2023-11-10 18:32:29 -0800

    finished styling add page

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -2,27 +2,56 @@ import 'package:flutter/material.dart';
 
 class AddPage extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
+    List<String> buttonNames = ['College 9/Lewis', 'Cowell/Stevenson', 'Crown/Merill', 'Porter/Kresge', 'Rachel Carson/Oakes', 'Scan Nutrition Label'];
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Nexa'),
       title: 'Cruz Nibbles',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Cruz Nibbles'),
+          title: const Text('Add Item'),
+          backgroundColor: Colors.green.shade300,
         ),
-        body: GridView.count(
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: GridView.count(
           // create two columns
           crossAxisCount: 2,
-          // Generate 5 widgets, one for each dining hall
-          children: List.generate(5, (index) {
+          // Generate 5 buttons, one for each dining hall
+          children: List.generate(6, (index) {
             return Center(
-              child: Text(
-                'Item $index',
-                style: Theme.of(context).textTheme.headlineSmall,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Don't need to touch anything before this
+                      // Add the link and function here
+                      // Remember this is a for loop
+                      print('Button $index pressed');
+
+
+
+                      // Don't need to touch anything after this
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0), // Adjust the value as needed
+                      ),
+                      fixedSize: Size(160,160),
+                      primary: Colors.green.shade300,
+                    ),
+                    child: Text(
+                      '${buttonNames[index]}',
+                      style: TextStyle(fontFamily: 'Nexa', fontSize: 20,),
+                    ),
+                  ),
+                  SizedBox(height: 8), // Optional spacing between button and text
+
+                ],
               ),
             );
-          }),
+          }),),
         ),
       ),
     );
