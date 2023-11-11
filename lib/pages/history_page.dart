57dc02b57255298f84a406f6fc31bd41acdc6091
Adam Owyang <>
2023-11-10 17:07:54 -0800

    wrote structure for history page

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -8,6 +8,34 @@ class HistoryPage extends StatefulWidget {
 class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('History Page'));
+    //return Center(child: Text('History Page'));
+    final List<Map<String, dynamic>> _items = List.generate(
+      10,
+        (index) => {
+        'id': index,
+          'title': 'Item $index',
+          'description':
+              'description for item $index',
+        }
+    );
+
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Nexa'),
+      home: Scaffold(
+        appBar: AppBar(title: Text("History"),),
+        body: SingleChildScrollView(
+          child: ExpansionPanelList.radio(
+            children: _items.map((e) => ExpansionPanelRadio(
+              value: e,
+              headerBuilder: (BuildContext context, bool isExpanded) => ListTile(
+                title: Text(e['title'].toString()),
+              ),
+              body: Container(
+                child: Text(e['description']),
+              ))).toList(),
+          ),
+        ),
+      ),
+    );
   }
 }
